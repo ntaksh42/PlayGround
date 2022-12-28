e512f3d4f93f2a5782a5bd9da492c2088ae807cb
naotoAkashi42 <naotoAkashi42@gmail.com>
2022-12-29 06:11:58 +0900

    Facadeパターン復習

---
+++ StudyDesignPattern/Machine/IMachineFacade.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Machine
+{
+    public interface IMachineFacade
+    {
+        int BoxInternalTemperature();
+        int BoxExternalTemperature();
+    }
+}
