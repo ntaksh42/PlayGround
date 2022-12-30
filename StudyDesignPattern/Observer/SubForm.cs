a88769a757803cea5085e16ef85685bdb711e00e
naoto akashi <aksh0402@outlook.jp>
2022-12-31 07:28:38 +0900

    observer

---
+++ StudyDesignPattern/Observer/SubForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Observer
+{
+    public partial class SubForm : Form
+    {
+        public SubForm()
+        {
+            InitializeComponent();
+
+            StartPosition = FormStartPosition.CenterScreen;
+
+            timer1.Interval = 5000;
+            timer1.Enabled = true;
+
+            WarningTimer.WarningStateChanged += WarningTimer_WarningStateChanged;
+            this.Disposed += SubForm_Disposed;
+        }
+
+        private void SubForm_Disposed(object sender, EventArgs e)
+        {
+            WarningTimer.WarningStateChanged -= WarningTimer_WarningStateChanged;
+        }
+
+        private void WarningTimer_WarningStateChanged(bool isWarning)
+        {
+            this.Invoke((Action)delegate ()
+            {
+                if (isWarning)
+                {
+                    WarningLabel.Text = "警報";
+                    WarningLabel.BackColor = Color.Red;
+                }
+                else
+                {
+                    WarningLabel.Text = "正常";
+                    WarningLabel.BackColor = Color.Lime;
+                }
+            });
+        }
+    }
+}
---
+++ StudyDesignPattern/Observer/WarningTimer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    public static class WarningTimer
+    {
+        private static System.Threading.Timer _timer;
+        public static event Action<bool> WarningStateChanged; 
+        static WarningTimer()
+        {
+            _timer = new System.Threading.Timer(TimerCallback);
+        }
+
+        private static bool _isWaring = false;
+        public static bool IsWarning
+        {
+            get { return _isWaring; } 
+            private set 
+            {   
+                if(_isWaring != value)
+                {
+                    _isWaring = value;
+                    WarningStateChanged?.Invoke(_isWaring);
+                }
+            }
+        }
+
+        public static void Start()
+        {
+            _timer.Change(0, 5000);
+        }
+
+        private static void TimerCallback(object state)
+        {
+            Console.WriteLine(Environment.CurrentDirectory);
+            var lines = System.IO.File.ReadAllLines("warning.txt");
+            if (lines.Length == 0) return;
+            IsWarning = (lines[0] == "1");
+        }
+    }
+}
