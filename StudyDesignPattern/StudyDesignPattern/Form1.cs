8d3779160c8a05e0cddba8abffbacaddd657c6e4
naoto akashi <aksh0402@outlook.jp>
2022-12-30 17:47:50 +0900

    シングルトン

--- StudyDesignPattern/StudyDesignPattern/Form1.cs
+++ StudyDesignPattern/StudyDesignPattern/Form1.cs
@@ -1,3 +1,4 @@
+
 using StudyDesignPattern.Objects;
 
 namespace StudyDesignPattern
---
+++ StudyDesignPattern/TImerSinglton/TimerChecker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TimerSinglton
+{
+    internal class TimerChecker
+    {
+        private readonly Timer _timer;
+
+        //private static TimerChecker _instance;
+        //public static TimerChecker GetInstance()
+        //{
+        //    return _instance ?? (_instance = new TimerChecker());
+        //}
+
+        public static TimerChecker Instance { get; } = new TimerChecker();
+
+        private  TimerChecker()
+        {
+            _timer = new Timer(TimerCallBack);
+        }
+
+        public  void Start()
+        {
+            _timer.Change(0, 5000);
+        }
+
+        private void TimerCallBack(object sender)
+        {
+            Console.WriteLine(DateTime.Now.ToString());
+        }
+    }
+}
