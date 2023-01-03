0095604dc0d9ef7f8024e91ebc4f0e5d9974fc99
naotoAkashi42 <naotoAkashi42@gmail.com>
2023-01-03 12:52:55 +0900

    DOM入門

--- StudyWeb/js-base/main.js
+++ StudyWeb/js-base/main.js
@@ -1,54 +1,29 @@
-// おまじない。
 'use strict'
 
-// let var が変数の宣言に使えるが、基本はletを使う。
-// letにだけ、ブロックスコープがある。
-// for (let i = 0; i < 10; i++) {
-//     console.log(i);
-// }
+const btnAdd = document.getElementById('btnAdd');
+btnAdd.addEventListener('click', () => {
+    const li = document.createElement('li');
+    let text = document.getElementById('textInput');
+    if(text.value === '') return ;
+    const textNode = document.createTextNode(text.value);
+    li.appendChild(textNode);
+    const list = document.getElementById('list');
+    list.appendChild(li);
+}, false);
 
-// 比較演算子
-// == : 比較。型が違う場合もTRUE
-// === : 厳密比較。型もチェックに入る。
-// let num = 100;
-// let str = '100';
-// if (num === str) {
-//     console.log('同じ');
-// }
+const selection = document.getElementById('selection');
+selection.addEventListener('click', () => {
 
-// 配列の宣言
-// let hoge = [10];
-// let colors = ['red', 'blue', 'green'];
-// colors.forEach(element => {
-//     console.log(element);
-// });
+    while(selection.lastChild){
+        selection.removeChild(selection.lastChild);
+    }
+    let newCnt = document.getElementsByTagName('li').length;
+    for (let index = 0; index < newCnt; index++) {
+        const newList = document.createElement('option');
+        let newOp = document.createTextNode(index);
+        newList.appendChild(newOp);
+        selection.appendChild(newList);
+    }
+});
 
-// 連想配列
-// hash dictionay mapなど
-// const peopleMap = {name:'A', gender:'man'}
-
-
-// function命令
-function calcArea(height, width) {
-    return height * width;
-}
-
-// 関数リテラル
-const f1 = function(height, width){
-    return height * width;
-}
-
-// Functionコンストラクタ
-const f2 = new Function('height', 'width', 'return height * width')
-console.log(f2(9,10));
-
-// アロー関数
-const f3 = (h, w) =>{
-    return h * w;
-}
-console.log(f3(3, 4));
-
-function calcPrice (num, unitPrice){
-    return num * unitPrice;
-}
-console.log(calcPrice(13, 100));
+// 置換を押したらItemが置き換わる。置き換え対象はドロップダウンで指定。
