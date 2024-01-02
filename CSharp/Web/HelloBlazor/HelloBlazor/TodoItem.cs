599890bb413244355256230dc3a700a1b6c8576d
naoto akashi <naotoAkashi42@gmail.com>
2024-01-02 14:32:39 +0900

    HelloBlazor

---
+++ CSharp/Web/HelloBlazor/HelloBlazor/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloBlazor
+{
+    public class TodoItem
+    {
+        public string? Title { get; set; }
+        public bool IsDone { get; set; }
+
+        public DateTime CreateTime { get; set; }
+    }
+}
